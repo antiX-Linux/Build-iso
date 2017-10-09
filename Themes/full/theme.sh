d2ef38609d7b6be5137959cd8a32c908772e1cf5
anticapitalista <antiX@operamail.com>
2017-10-09 13:20:41 +0300

    latest-antiX-17-rc2

--- Themes/full/theme.sh
+++ Themes/full/theme.sh
@@ -4,31 +4,36 @@ THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
 
+copy_file alsamixer.desktop 		/usr/share/applications/antix/
+copy_file speaker-test.desktop 	/usr/share/applications/antix/
+copy_file sysv-rc-conf.desktop 	/usr/share/applications/antix/
+copy_file run.desktop 			/usr/share/applications/antix/
+copy_file xkill.desktop 			/usr/share/applications/antix/
 #copy_file menu-applications 		/usr/share/desktop-menu/.jwm/  ##base only
 #copy_file menu-applications-fb 	/usr/share/desktop-menu/.fluxbox/  ##base only
 #copy_file menu-applications-ice 	/usr/share/desktop-menu/.icewm/  ##base only
-copy_file arandr.desktop 		/usr/share/applications/
+#copy_file arandr.desktop 		/usr/share/applications/
 copy_file ceni.desktop 			/usr/share/applications/
-copy_file galternatives.desktop 	/usr/share/applications/
-copy_file gnome-ppp.desktop 		/usr/share/applications/
-copy_file gparted.desktop 		/usr/share/applications/antix/
-copy_file grsync.desktop 		/usr/share/applications/
+#copy_file galternatives.desktop 	/usr/share/applications/
+#copy_file gnome-ppp.desktop 		/usr/share/applications/
+#copy_file gparted.desktop 		/usr/share/applications/antix/
+#copy_file grsync.desktop 		/usr/share/applications/
 #copy_file hardinfo.desktop 		/usr/share/applications/
 #copy_file live-kernel-updater.desktop	/usr/share/applications/antix/
 #copy_file live-usb-maker-gui.desktop	/usr/share/applications/antix/
 copy_file luckybackup.desktop 		/usr/share/applications/
 copy_file luckybackup-gnome-su.desktop 	/usr/share/applications/
 #copy_file metapackage-installer.desktop	/usr/share/applications/antix/ 
-copy_file ndisgtk.desktop 		/usr/share/applications/
+#copy_file ndisgtk.desktop 		/usr/share/applications/
 copy_file pybootchartgui.desktop 	/usr/share/applications/
-copy_file rxvt-unicode.desktop 		/usr/share/applications/
+#copy_file rxvt-unicode.desktop 		/usr/share/applications/
 copy_file synaptic.desktop 		/usr/share/applications/antix/
-copy_file system-config-printer.desktop /usr/share/applications/
-copy_file umts-panel.desktop 		/usr/share/applications/
+#copy_file system-config-printer.desktop /usr/share/applications/
+#copy_file umts-panel.desktop 		/usr/share/applications/
 copy_file vim.tiny.desktop 		/usr/share/applications/
-copy_file wicd.desktop 			/usr/share/applications/
-copy_file wpa_gui.desktop 		/usr/share/applications/
-copy_file xfburn.desktop 		/usr/share/applications/
+#copy_file wicd.desktop 			/usr/share/applications/
+#copy_file wpa_gui.desktop 		/usr/share/applications/
+#copy_file xfburn.desktop 		/usr/share/applications/
 copy_file calcurse.desktop 		/usr/share/applications/antix/
 copy_file nano.desktop 			/usr/share/applications/
 copy_file ddm-ati.desktop 		/usr/share/applications/
@@ -36,7 +41,7 @@ copy_file ddm-nvidia.desktop 		/usr/share/applications/
 copy_file display-im6.desktop		/usr/share/applications/ 
 copy_file display-im6.q16.desktop	/usr/share/applications/
 copy_file mcedit.desktop 		/usr/share/applications/
-copy_file pppoeconf.desktop 		/usr/share/applications/
+#copy_file pppoeconf.desktop 		/usr/share/applications/
 copy_file mps-youtube.desktop 		/usr/share/applications/
 
 copy_file lxde-audio-video.directory 	/usr/share/desktop-directories/
